4aa3acd69f0199635e63738c032f4a42acfc176a
Jan Sikora <sikorkaj25@outlook.com>
2026-08-16 08:41:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,7 @@ app.get('/api/books/isbn', async (req, res) => {
     const bookcaseId = req.query.id;
     try {
         const response = await fetch(`https://www.knihovny.cz/api/v1/search?lookfor=isbn:${isbn}&field[]=authors&field[]=title&field[]=humanReadablePublicationDates&field[]=bibliographicLevel&field[]=physicalDescriptions&sort=relevance&limit=2`);
+        console.log(response)
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
